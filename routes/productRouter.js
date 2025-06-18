2b590dca2b5f7215c9bc97e702d4589da59d1901
chamika <chamikaheshani1155@gmail.com>
2025-06-18 20:17:21 +0530

    day 21

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/productController.js';
+import { createProduct, deleteProduct, getProductById, getProducts, searchProduct, updateProduct } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProducts)
+productRouter.get("/search/:id",searchProduct)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
 productRouter.get("/:id" ,getProductById)
+
 export default productRouter;
